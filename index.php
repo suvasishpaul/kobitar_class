9cdca5d3c0c8bd35f6437b8a35a3426affeb8f85
Suvasish Paul <suvasishpaul@gmail.com>
2016-08-12 23:32:22 +0600

    All file added

---
+++ index.php
@@ -0,0 +1,448 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <meta name="format-detection" content="telephone=no"/>
+    <link rel="icon" href="images/favicon.ico" type="image/x-icon">
+    <title>HOME</title>
+
+    <!-- Bootstrap -->
+    <link href="css/bootstrap.css" rel="stylesheet">
+
+    <!-- Links -->
+    <link rel="stylesheet" href="css/camera.css">
+    <link rel="stylesheet" href="css/search.css">
+    <link rel="stylesheet" href="css/google-map.css">
+
+
+    <!--JS-->
+    <script src="js/jquery.js"></script>
+    <script src="js/jquery-migrate-1.2.1.min.js"></script>
+    <script src="js/rd-smoothscroll.min.js"></script>
+
+
+    <!--[if lt IE 9]>
+    <div style=' clear: both; text-align:center; position: relative;'>
+        <a href="http://windows.microsoft.com/en-US/internet-explorer/..">
+            <img src="images/ie8-panel/warning_bar_0000_us.jpg" border="0" height="42" width="820"
+                 alt="You are using an outdated browser. For a faster, safer browsing experience, upgrade for free today."/>
+        </a>
+    </div>
+    <script src="js/html5shiv.js"></script>
+    <![endif]-->
+    <script src='js/device.min.js'></script>
+  </head>
+  <body>
+  <div class="page">
+  <!--========================================================
+                            HEADER
+  =========================================================-->
+    <header>  
+      <div class="container top-sect">
+        <div class="navbar-header">
+          <h1 class="navbar-brand">
+            <a data-type='rd-navbar-brand'  href="./">কবিতার ক্লাশ</a>
+          </h1>
+          <a class="search-form_toggle" href="#"></a>
+        </div>
+
+        <div class="help-box text-right">
+          <p>বাংলা কবিতা ও আবৃত্তি বিষয়ক সম্পূর্ণ  ওয়েব পোর্টাল</p>
+          <p>শুদ্ধ উচ্চারণ বাচিক শিল্প ও উপসথাপনা</p>
+
+        </div>
+      </div>
+
+
+      <div id="stuck_container" class="stuck_container">
+        <div class="container">   
+        <nav class="navbar navbar-default navbar-static-top pull-left">            
+
+            <div class="">  
+              <ul class="nav navbar-nav sf-menu" data-type="navbar">
+                <li class="active">
+                  <a href="./">নীড়পাতা</a>
+                </li>
+                <li>
+                  <a href="index-1.html">আবৃত্তি</a>
+                </li>
+                <li class="dropdown">
+                  <a href="index-2.html">কবিতা<span class="glyphicon glyphicon-menu-down" aria-hidden="true"></span></a>
+                  <ul class="dropdown-menu">
+                    <li>
+                      <a href="#">দ্রোহ</a>
+                    </li>
+                    <li>
+                      <a href="#">প্রেম </a>                      
+                    </li>
+                    <li>
+                      <a href="#">ইতিহাসভিত্তিক </a>
+                        <ul class="dropdown-menu">
+                          <li>
+                            <a href="#">সাম্প্রতিক</a>
+                          </li>
+                          <li>
+                            <a href="#">পুরাতন</a>
+                          </li>                      
+                        </ul>
+                    </li>
+                  </ul>
+                </li>                
+                <li>
+                  <a href="index-3.html">ভিডিও</a>
+                </li>
+                
+                <li>
+                  <a href="index-4.html">যোগাযোগ</a>
+                </li>
+              </ul>                           
+            </div>
+        </nav>   
+        <form class="search-form" action="search.php" method="GET" accept-charset="utf-8">
+          <label class="search-form_label">
+            <input class="search-form_input" type="text" name="s" autocomplete="off" placeholder="পছন্দের বিষয় খুজুন"/>
+            <span class="search-form_liveout"></span>
+          </label>
+          <button class="search-form_submit fa-search" type="submit"></button>
+        </form>
+             
+        </div>
+
+      </div>  
+    </header>
+
+  <!--========================================================
+                            CONTENT
+  =========================================================-->
+
+    <main>        
+
+      <section class="well well1 well1_ins1">
+        <div class="camera_container">
+          <div id="camera" class="camera_wrap">
+            <div data-src="images/page-1_slide1.jpg">
+              <div class="camera_caption fadeIn">
+                <div class="jumbotron jumbotron1">
+                  <em>
+                    আবৃত্তি ভাবনা
+                  </em>
+                  <div class="wrap">
+                    <p>
+                      প্রতিথযশা আবৃত্তিশিল্পীদের মনন ও ভাবনা নিয়ে এই আয়োজন<span> প্রতিথযশা আবৃত্তিশিল্পীদের মনন ও ভাবনা নিয়ে এই আয়োজন</span>.
+                    </p>
+                    <a href="#" class="btn-link fa-angle-right"></a>
+                  </div>  
+                </div>
+              </div>
+            </div>
+
+
+            <div data-src="images/page-1_slide2.jpg">
+              <div class="camera_caption fadeIn">
+                <div class="jumbotron jumbotron2">
+                  <em>
+                    আবৃত্তির ক্যাসেট
+                  </em>
+                  <div class="wrap">
+                    <p>
+                      নবীন ও উদীয়মান আবৃত্তিশিল্পীদের অনুপ্রাণিত এবং তাদের আবৃত্তি সবার মাঝে ছড়িয়ে দিতে আমাদের এই প্রয়াস<span> নবীন ও উদীয়মান আবৃত্তিশিল্পীদের অনুপ্রাণিত এবং তাদের আবৃত্তি সবার মাঝে ছড়িয়ে দিতে আমাদের এই প্রয়াস</span>.
+                    </p>
+                    <a href="#" class="btn-link hov_prime fa-angle-right"></a>
+                  </div>  
+                </div>
+              </div>
+            </div>
+            <div data-src="images/page-1_slide3.jpg">
+              <div class="camera_caption fadeIn">
+                <div class="jumbotron">
+                  <em>
+                    আবৃত্তির ক্লাশ
+                  </em>
+                  <div class="wrap">
+                    <p>
+                      আবৃত্তিকে সবার মাঝে ছড়িয়ে দিতে অনলাইনে এই ক্লাশের ব্যবস্থা করা হয়েছে<span> আবৃত্তিকে সবার মাঝে ছড়িয়ে দিতে অনলাইনে এই ক্লাশের ব্যবস্থা করা হয়েছে</span>.
+                    </p>
+                    <a href="#" class="btn-link fa-angle-right"></a>
+                  </div>  
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+
+        <div class="container center991">
+          <div class="row">
+            <div class="col-md-4 col-sm-12 col-xs-12">
+              <div class="thumbnail thumb-shadow">
+                <img src="images/page-1_img1.jpg" alt="">
+                <div class="caption bg2">
+                  <h3>
+                    অডিও সিডি সমূহ
+                  </h3>
+                  <div class="wrap">
+                    <p>
+                      দেশের খ্যাতনামা সকল আবৃত্তিশিল্পীর অডিও এলবাম একসাথে
+                    </p>
+                    <a href="#" class="btn-link fa-angle-right"></a>
+                  </div>  
+                </div>
+              </div>              
+            </div>
+            <div class="col-md-8 col-sm-12 col-xs-12">
+              <div class="thumbnail thumb-shadow">
+                <img src="images/page-1_img2.jpg" alt="">
+                <div class="caption bg3">
+                  <h3>
+                    আবৃত্তির সকল সংবাদ একসাথে
+                  </h3>
+                  <div class="wrap">
+                    <p class="thumb_ins1">
+                      দেশের প্রতিটি প্রান্তে ঘটে যাওয়া সকল আবৃত্তির সংবাদ একসাথে এই প্রথম.
+                    </p>
+                    <a href="#" class="btn-link fa-angle-right"></a>
+                  </div>  
+                </div>
+              </div> 
+            </div>
+          </div>
+
+          <div class="row wow fadeIn" data-wow-duration='2s'>
+            <div class="col-md-4 col-sm-12 col-xs-12">
+              <div class="thumbnail thumb-shadow">
+                <img src="images/page-1_img3.jpg" alt="">
+                <div class="caption bg3">
+                  <h3>
+                    উচ্চারণ ক্লাশ
+                  </h3>
+                  <div class="wrap">
+                    <p>
+                      শুদ্ধ উচ্চারণ শিখুন দেশের প্রতিথযশা আবৃত্তিশিল্পীদের কাছ থেকে.
+                    </p>
+                    <a href="#" class="btn-link fa-angle-right"></a>
+                  </div>  
+                </div>
+              </div>              
+            </div>
+            <div class="col-md-4 col-sm-12 col-xs-12">
+              <div class="thumbnail thumb-shadow">
+                <img src="images/page-1_img4.jpg" alt="">
+                <div class="caption bg-primary">
+                  <h3>
+                    আবৃত্তি ভাবনা
+                  </h3>
+                  <div class="wrap">
+                    <p>
+                      আবৃত্তি নিয়ে কে কি ভাবছেন ?.
+                    </p>
+                    <a href="#" class="btn-link fa-angle-right"></a>
+
+                  </div>
+                </div>
+              </div>              
+            </div>
+            <div class="col-md-4 col-sm-12 col-xs-12">
+              <div class="thumbnail thumb-shadow">
+                <img src="images/page-1_img5.jpg" alt="">
+                <div class="caption bg2">
+                  <h3>
+                    ণির্শান ভাবনা
+                  </h3>
+                  <div class="wrap">
+                    <p>
+                      কবিতা আবৃত্তির আগে তার অর্ন্তনিহিত ভাব বোঝা খুবই প্রয়োজন  ণির্শণ ভাবনা ছাড়া কবিতা অর্থহীণ.
+                    </p>
+                    <a href="#" class="btn-link fa-angle-right"></a>
+                  </div>  
+                </div>
+              </div>              
+            </div>
+          </div>
+        </div>        
+      </section>
+
+      <section class="well well2">
+        <div class="container">
+          <h2>
+            আবৃত্তির ক্যাসেট সমূহ
+
+          </h2>
+          <div class="row offs1">
+            <div class="col-md-6 col-sm-12">
+              <ul class="link-list wow fadeInLeft" data-wow-duration='3s'>
+                <li>
+
+                  <a href="#">সন্জয় পাল- আমারে তুমি অশেষ করেছো </a>
+
+                  <a href="#" class="btn-link l-h1 fa-angle-right"></a>
+                </li>
+                <li>
+
+                  <a href="#">জাভেদ হোসেন - মেঘলা রঙয়ের চিঠি </a>
+
+                  <a href="#" class="btn-link l-h1 fa-angle-right"></a>
+                </li>
+                <li>
+
+                  <a href="#">শারমিন মৃত্তিকা- আমি প্রীতিলতা বলছি</a>
+
+                  <a href="#" class="btn-link l-h1 fa-angle-right"></a>
+                </li>
+                <li>
+
+                  <a href="#">সোহেল আনোয়ার- হে অনার্য পিতা</a>
+
+                  <a href="#" class="btn-link l-h1 fa-angle-right"></a>
+                </li>
+              </ul>
+            </div>
+            <div class="col-md-6 col-sm-12">
+              <img class="width_img" src="images/page-1_img6.jpg" alt="">
+            </div>
+          </div>
+        </div>
+      </section>
+      
+      <section class="well well2 wow fadeIn  bg1" data-wow-duration='3s'>
+        <div class="container">
+        <h2 class="txt-pr">
+          কবিতার ক্লাশের পথচলা
+        </h2>
+          <div class="row">
+            <div class="col-md-6 col-sm-12">
+              <p>
+                কবিতার ক্লাশ একটি ব্যতিক্রমধর্মী উদ্যোগ বাংলাভাষীদের কবিতার প্রতি আগ্রহী করে তুলতে এই আয়োজন
+                কবিতার ক্লাশ একটি ব্যতিক্রমধর্মী উদ্যোগ বাংলাভাষীদের কবিতার প্রতি আগ্রহী করে তুলতে এই আয়োজন
+                কবিতার ক্লাশ একটি ব্যতিক্রমধর্মী উদ্যোগ বাংলাভাষীদের কবিতার প্রতি আগ্রহী করে তুলতে এই আয়োজন
+
+              </p>
+            </div>
+            <div class="col-md-6 col-sm-12">
+              <p>
+                কবিতার ক্লাশ একটি ব্যতিক্রমধর্মী উদ্যোগ বাংলাভাষীদের কবিতার প্রতি আগ্রহী করে তুলতে এই আয়োজন
+                কবিতার ক্লাশ একটি ব্যতিক্রমধর্মী উদ্যোগ বাংলাভাষীদের কবিতার প্রতি আগ্রহী করে তুলতে এই আয়োজন
+                কবিতার ক্লাশ একটি ব্যতিক্রমধর্মী উদ্যোগ বাংলাভাষীদের কবিতার প্রতি আগ্রহী করে তুলতে এই আয়োজন
+
+
+
+                <a href="#" class="btn-link l-h1 fa-angle-right"></a>
+              </p>
+            </div>
+          </div>
+        </div>
+      </section>
+
+
+
+      <section class="well well3 parallax" data-url="images/parallax1.jpg" data-mobile="
+      true" data-speed="0.9">
+        <div class="container">
+          <div class="wrap text-center">
+            <strong>
+              SAVE TIME,<br />
+              SAVE MONEY,
+              <small>
+                GROW & SUCCEED
+              </small>
+            </strong>
+            <p>
+              Lorem ipsum dolor sit amet conse ctetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip.
+            </p>
+            <a href="#" class="btn btn-primary">read more <span class="fa-angle-right"></span></a>
+          </div>  
+        </div>        
+      </section>
+
+      <section class="well well2">
+        <div class="container">
+          <h2>
+            our 
+            <small>
+              clients
+            </small>
+          </h2>
+
+          <div class="row offs1">
+            <ul class="flex-list">
+              <li class="col-lg-3 col-sm-3 col-xs-6">
+                <a href="#">
+                  <img src="images/page-1_img7.jpg" alt="">
+                </a>  
+              </li>
+              <li class="col-lg-3 col-sm-3 col-xs-6">
+                <a href="#">
+                  <img src="images/page-1_img8.jpg" alt="">
+                </a>  
+              </li>
+              <li class="col-lg-3 col-sm-3 col-xs-6">
+                <a href="#">
+                  <img src="images/page-1_img9.jpg" alt="">
+                </a>  
+              </li>
+              <li class="col-lg-3 col-sm-3 col-xs-6">
+                <a href="#">
+                  <img src="images/page-1_img10.jpg" alt="">
+                </a>  
+              </li>
+            </ul>
+
+            <ul class="flex-list">
+              <li class="col-lg-3 col-sm-3 col-xs-6">
+                <a href="#">
+                  <img src="images/page-1_img11.jpg" alt="">
+                </a>  
+              </li>
+              <li class="col-lg-3 col-sm-3 col-xs-6">
+                <a href="#">
+                  <img src="images/page-1_img12.jpg" alt="">
+                </a>  
+              </li>
+              <li class="col-lg-3 col-sm-3 col-xs-6">
+                <a href="#">
+                  <img src="images/page-1_img13.jpg" alt="">
+                </a>  
+              </li>
+              <li class="col-lg-3 col-sm-3 col-xs-6">
+                <a href="#">
+                  <img src="images/page-1_img14.jpg" alt="">
+                </a>  
+              </li>
+            </ul>
+          </div>  
+          
+        </div>
+      </section>
+
+    </main>
+
+    <!--========================================================
+                            FOOTER
+  =========================================================-->
+  <footer class="">
+
+
+    <section class="well1">
+      <div class="container"> 
+            <p class="rights">
+              Business Company  &#169; <span id="copyright-year"></span>
+              <a href="index-5.html">Privacy Policy</a>
+              More Business Website Templates at <a rel="nofollow" href="http://www.templatemonster.com/category/business-web-templates/" target="_blank">TemplateMonster.com</a>
+            </p>          
+      </div> 
+    </section>    
+  </footer>
+  </div>
+
+
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <!-- Include all compiled plugins (below), or include individual files as needed -->         
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/tm-scripts.js"></script>    
+  <!-- </script> -->
+
+
+  </body>
+</html>
